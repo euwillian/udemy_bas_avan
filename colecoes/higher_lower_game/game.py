cd5d1bac50928b488cb2124386fbe0d3a7d5cbfc
Willian Santos <wiicontatos@gmail.com>
2025-10-21 18:28:38 -0300

    final version of the game

--- colecoes/higher_lower_game/game.py
+++ colecoes/higher_lower_game/game.py
@@ -1,28 +1,65 @@
-
-
 import game_data # acessa a lista/dicionário
 import random
 
+def compare(pessoa_a: dict, pessoa_b: dict, escolha: str) -> bool:
+    """Retorna True se o usuário acertou, False caso contrário."""
+    a = pessoa_a['follower_count']
+    b = pessoa_b['follower_count']
 
-# Acessar dados game_data de forma aleatória, porém preciso sempre salvar em uma variável quem estou comparando com quem A > B > C > D
-
-pessoa_a = random.choice(game_data.data)
-print(pessoa_a) # dicionário da pessoa A
-
-pessoa_b = random.choice(game_data.data)
-print(pessoa_b) 
-
-
-# for chave, valor in enumerate(game_data.data):
-#     print(chave, valor['name'], valor['follower_count'], valor['description'], valor['country'])
-
+    if escolha == 'A':
+        return a > b # True
+    elif escolha == 'B':
+        return b > a # True
+    else:
+        return False
 
 
-# Perguntar ao usúario quem tem mais seguidores A ou B ?
+def random_person() -> dict:
+    """Retorna um dicionário de uma pessoa aleatória"""
+    return random.choice(game_data.data)
+
+
+def format_person(pessoa: dict) -> str:
+    """Formata a exibição da pessoa."""
+    return f"{pessoa['name']}, {pessoa['description']}, from {pessoa['country']}"
+
+
+def main():
+    score = 0
+    
+    pessoa_a = random_person()
+    
+    while True:
+        
+        pessoa_b = random_person()
+        
+        # Evita que A e B sejam a mesma pessoa
+        while pessoa_b == pessoa_a:
+            pessoa_b = random_person()
+        
+        # apresentar
+        print(f"Compare A: {format_person(pessoa_a)} \n")
+        print(f"VS \n")
+        print(f"Compare B: {format_person(pessoa_b)}")
+        
+        # usuario informa qual tem mais seguidores
+        escolha = input("Who has more followers? Type 'A' or 'B': ").upper()
+        
+        if compare(pessoa_a, pessoa_b, escolha):
+            score += 1
+            print(f"You're right! Current score: {score}")
+            # Ganhou o B vira o próximo A
+            pessoa_a = pessoa_b
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+    
+    
+if __name__ == "__main__":
+    main()
 
 
 
-# Sistema de pontuação se acertar continua se errar finaliza
 
 
-# Posso repetir? Aparentemente é
+
